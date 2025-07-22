53162b9058c2c7867e7264ba41f2a360390a281c
islam <lameche1islam@gmail.com>
2025-07-22 21:05:50 +0100

    completely removed comments featurer :(

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/minimalist-portfolio",
+  assetPrefix: "/minimalist-portfolio/",
+  reactStrictMode: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
