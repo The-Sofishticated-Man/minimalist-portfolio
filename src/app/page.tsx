b5d8d3e4b566e21242bd038b9f9cd210af9ed613
islam <lameche1islam@gmail.com>
2025-07-26 16:07:10 +0100

    Wrap Home component return in a div for consistent layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import SecondaryInfo from "../components/SecondaryInfo";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col lg:flex-row min-h-screen">
       <MainInfo />
       <SecondaryInfo />
-    </>
+    </div>
   );
 }
